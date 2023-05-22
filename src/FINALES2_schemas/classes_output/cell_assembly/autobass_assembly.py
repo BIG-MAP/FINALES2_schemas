40bbaa504735263cafac85e85e716859f9790ad3
Bojing <75305395+Bojing4313@users.noreply.github.com>
2023-05-22 18:37:25 +0200

    autobass_schema#2 update

---
+++ src/FINALES2_schemas/classes_output/cell_assembly/autobass_assembly.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from uuid import UUID
+from .minimal_output import AssemblyOutput
+
+class AutoBASSOutput(BaseModel):
+    """
+    Result returned from the cell assembly, which merges all the cells in on batch:
+    """
+    batch_output:list[AssemblyOutput] = Field(
+        description="The list includes the AssemblyOutput class from each single cell."
+    )
+    batch_id: UUID = Field(
+        description="A unique identifier assigned to this cell in this run."
+    )
