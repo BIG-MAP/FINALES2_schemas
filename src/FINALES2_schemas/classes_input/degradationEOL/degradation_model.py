64c016bfc62a2e06991c57978edd436c878eb353
Laura Rieger <lauri@dtu.dk>
2023-06-28 11:02:32 +0200

    updated input to update input_cycles and remove input not needed

--- src/FINALES2_schemas/classes_input/degradationEOL/degradation_model.py
+++ src/FINALES2_schemas/classes_input/degradationEOL/degradation_model.py
@@ -8,29 +8,9 @@ class DegradationModelInput(BaseModel):
     :param BaseModel: _description_
     :type BaseModel: _type_
     """
-    chemistry:BatteryChemistry = Field(
-        description=("The description of the chemicals involved in all the battery cell.")
-    )
-    input_cycles:int = Field(
-        description=("The number of cycles provided as an input to the model.")
-    )
-    average_charging_rate:float = Field(
-        unit=str(unit_registry.h **-1),
-        description=("The average C-rate used for charging of the cell in the cycles "
-                     "provided as an input. "
-                     f"Unit: {str(unit_registry.h **-1)}")
-    )
-    maximum_charging_rate:float = Field(
-        unit=str(unit_registry.h **-1),
-        description=("The maximum C-rate used for charging of the cell in the cycles "
-                     "provided as an input. "
-                     f"Unit: {str(unit_registry.h **-1)}")
-    )
-    minimum_charging_rate:float = Field(
-        unit=str(unit_registry.h **-1),
-        description=("The minimum C-rate used for charging of the cell in the cycles "
-                     "provided as an input. "
-                     f"Unit: {str(unit_registry.h **-1)}")
+
+    input_cycles:list = Field(
+        description=("The capacities for the input cycles provided as an input to the model.")
     )
     delta_coulombic_efficiency:float = Field(
         description=("The difference in coulombic efficiency between cycle x and 10.")
@@ -44,9 +24,4 @@ class DegradationModelInput(BaseModel):
     capacity_vector_variance:float = Field(
         description=("The variance of the difference between the Q(V) curves between "
                      "cycle x and 10.")
-    )
-    data_path:str = Field(
-        description=("The path, where the data wo work on can be accessed. This is "
-                     "relevant as the data files are large and cannot easily be sent "
-                     "via FINALES.")
     )
