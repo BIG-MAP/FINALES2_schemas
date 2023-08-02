473fa95489e59ee2bce069cb84e7964cd82fcdcf
MonikaVo <monika.vogler@kit.edu>
2023-08-02 10:19:08 +0000

    Minor adaptions

--- src/FINALES2_schemas/classes_output/capacity/minimal_output.py
+++ src/FINALES2_schemas/classes_output/capacity/minimal_output.py
@@ -6,10 +6,10 @@ class CapacityCyclingOutput(BaseModel):
     Results returned from the following quantities:
     `capacity` - `cycling`
     """
-    capacity_list: List = Field(
+    capacity_list: List[float] = Field(
         units="Ah",
         description="A list with calculated capacities in Ah for charge and discharge alternately."
     )
-    channel_list: List = Field(
+    channel_list: List[int] = Field(
         description="A list with the channels used for cycling."
     )
