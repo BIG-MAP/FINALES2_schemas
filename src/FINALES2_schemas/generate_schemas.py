a71642be3342fee1c81171ecac81c8dda89d0572
MonikaVo <79857021+MonikaVo@users.noreply.github.com>
2023-08-10 08:09:45 +0200

    Changed the cell_assembly filename to autobass_assembly (#19)
    - to have the naming consistent with the other quantities and methods

--- src/FINALES2_schemas/generate_schemas.py
+++ src/FINALES2_schemas/generate_schemas.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     quantity_path = BASEPATH_QUANTITIES / 'degradationEOL' / 'degradation_model.json'
     generate_quantity('degradationEOL', 'degradation_model', degradationEOL_input.DegradationModelInput, degradationEOL_output.DegradationModelOutput, quantity_path)
 
-    quantity_path = BASEPATH_QUANTITIES / 'cell_assembly' / 'cell_assembly.json'
+    quantity_path = BASEPATH_QUANTITIES / 'cell_assembly' / 'autobass_assembly.json'
     generate_quantity('cell_assembly', 'autobass_assembly', assembly_input.AssemblyInput, assembly_output.AutoBASSOutput, quantity_path)
 
     quantity_path = BASEPATH_QUANTITIES / 'electrolyte' / 'flow.json'
